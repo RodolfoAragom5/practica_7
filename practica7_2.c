0298fee8bd9be1b11ee47734c22cb6538a84e319
RodolfoAragom5 <143585799+RodolfoAragom5@users.noreply.github.com>
2024-09-27 14:17:25 -0600

    Update practica7_2.c

--- practica7_2.c
+++ practica7_2.c
@@ -1,1 +1,26 @@
-
+#include <stdio.h>
+int main(){
+  char op='\0';
+  printf("\tMenú\n\n");
+  printf("Elegir la opción deseada\n");
+  printf("a) Ingresar \n");
+  printf("b) Registrarse\n");
+  printf("c) Salir \n");
+  scanf("%c", &op);
+  switch(op)
+  {
+    Default:
+      printf("Opción no valida. \n");
+      break;
+        case 'a':
+          printf("Se seleccinó 'ingresar´.\n");
+          break;
+        case 'b':
+          printf("se seleccionó 'Resgistrarse'.\n");
+          break;
+        case 'c':
+          printf("Se seleccionó 'salir'. \n");
+          break;
+  }
+return 0;
+}
