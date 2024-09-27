37035ab24d885d6f27b52f401e1cd3195c7838fd
RodolfoAragom5 <143585799+RodolfoAragom5@users.noreply.github.com>
2024-09-27 14:46:40 -0600

    Update nueva_cal.c

--- nueva_cal.c
+++ nueva_cal.c
@@ -1,1 +1,34 @@
+/*Rodolfo Aragom Romero Sandoval 
+Ejercicio 4 calculadora con funciones 
+Práctica 7 27/09/24*/
 
+#include <stdio.h>
+#include <math.h>
+
+int suma(int a, int b)
+{
+  resp= a + b;
+  return(resp);
+}
+int resta(int a, int b)
+{
+  resp= a - b;
+  return(resp);
+}
+int mult(int a, int b)
+{
+  resp= a * b;
+  return(resp);
+}
+int divi(int a, int b)
+{
+  resp= a / b;
+  return(resp);
+}
+int pot(int a, int b)
+{
+  resp= pow(a,b);
+  return(resp);
+}
+
+int main()
